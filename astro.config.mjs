f6a0fe857659f3692e62b8b49ee8d354a2c990fb
matheusghyino <matheusghygino@gmail.com>
2025-10-31 16:59:05 -0300

    acertando envio do formulário para o fluxo n8n e adicionando thanks page inacabada

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,24 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
+
+import sitemap from '@astrojs/sitemap';
+
+
 export default defineConfig({
+  site: "https://scalyns.com",
+  integrations: [tailwindcss(), sitemap({
+    changefreq: "weekly",
+    priority: 0.8,
+    lastmod: new Date(),
+    customPages: [
+    ],
+  })],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    define: {
+      "import.meta.env.PUBLIC_FORM_API_KEY": JSON.stringify(process.env.PUBLIC_FORM_API_KEY)
+    },
   },
-  integrations: [
-    preact({ compat: false })
-  ],
+  integrations: [preact({ compat: false }), sitemap()],
 });
