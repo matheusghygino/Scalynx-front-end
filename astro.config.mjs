b8e21fe0043db6eace0ac24a0ea249b37c8621a1
matheusghyino <matheusghygino@gmail.com>
2026-03-04 18:44:48 -0300

    alterando config.mjs para melhor otimização e também a logo no header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,44 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import tailwindcss from '@tailwindcss/vite';
-
-
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+
   site: "https://scalyns.com",
-  integrations: [tailwindcss(), sitemap({
-    changefreq: "weekly",
-    priority: 0.8,
-    lastmod: new Date(),
-    customPages: [
-    ],
-  })],
+
+  integrations: [
+    preact({ compat: false }),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.8,
+      lastmod: new Date()
+    })
+  ],
+
   vite: {
     plugins: [tailwindcss()],
+
     define: {
-      "import.meta.env.PUBLIC_FORM_API_KEY": JSON.stringify(process.env.PUBLIC_FORM_API_KEY)
+      "import.meta.env.PUBLIC_FORM_API_KEY": JSON.stringify(
+        process.env.PUBLIC_FORM_API_KEY
+      )
     },
+
+    build: {
+
+      /* evita múltiplos CSS bloqueando render */
+      cssCodeSplit: false,
+
+      /* melhor compressão do bundle */
+      minify: "esbuild"
+
+    }
+
   },
-  integrations: [preact({ compat: false }), sitemap()],
+
+  /* melhora performance geral */
+  compressHTML: true
+
 });
